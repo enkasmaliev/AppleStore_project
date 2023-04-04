9f26e030bcfc25ed9e77f53c5012eeed39eaf7bb
Юсу Салихаров <yususaliharov@MacBook-Air.local>
2023-04-04 20:57:57 +0600

    Items

--- article/models.py
+++ article/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
 
     class Meta:
-        verbose_name = 'Продукция'
-        verbose_name_plural = 'Продукция'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
     
     def __str__(self) -> str:
         return self.name
@@ -18,11 +21,31 @@ class Item(models.Model):
         ('512 GB', '512 gb'),
         ('1 TB', '1 tb'),
     )
+    COLOR_CHOICES = (
+        ('BLACK', 'black'),
+        ('GOLD', 'gold'),
+        ('SILVER', 'silver'),
+    )
+    STATUS_CHOICES = (
+        ('in stock', 'В наличии'),
+        ('out of stock', 'Нет в наличии'),
+    )
 
+    categories = models.ManyToManyField(Category, related_name='items')
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
-    color = models.CharField(max_length=50)
+    color = models.CharField(max_length=50, choices=COLOR_CHOICES)
     memory = models.CharField(max_length=6, choices=MEMORY_CHOICES)
-    is_sold = models.BooleanField(default=False)
-    image = models.ImageField(upload_to='articles', null=True, blank=True)
+    image = models.ImageField(upload_to='items', null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='items', default=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='out of stock')
+
+
+    class Meta:
+        verbose_name = 'Продукция'
+        verbose_name_plural = 'Продукции'
+        ordering = ['price']
+
+    def __str__(self) -> str:
+        return self.name
--- article/views.py
+++ article/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Category, Item
+from .serializers import CategorySerializer, ItemSerializer
 
-# Create your views here.
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ItemViewSet(ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({'request': self.request})
+        return context
